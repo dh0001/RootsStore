f5a4bc8dee131384cf59fc167e8a3676238c92e5
hujoseph99 <hujoseph99@gmail.com>
2023-02-04 15:13:13 -0500

    refactor physics process code

--- Player.gd
+++ Player.gd
@@ -3,9 +3,8 @@ extends KinematicBody2D
 onready var _animated_sprite = $AnimatedSprite
 export var speed = 400 # How fast the player will move (pixels/sec).
 
-func _physics_process(delta):
+func get_velocity():
 	var velocity = Vector2.ZERO
-	
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -14,14 +13,18 @@ func _physics_process(delta):
 		velocity.y -= 1
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
-		
-	velocity = velocity.normalized() * speed
-	var motion = velocity * delta 
-	
+	return velocity.normalized() * speed
+
+func set_animation(motion):
 	if test_move(self.transform, motion):
 		_animated_sprite.animation = "walk"
 	else:
 		_animated_sprite.animation = "stand"
 	_animated_sprite.play()
 
+func _physics_process(delta):
+	var velocity = get_velocity()
+	var motion = velocity * delta 
+	
+	set_animation(motion)
 	move_and_collide(motion)
