4193b774dc5a73c02d38aac2bb996c1947bfef3d
hujoseph99 <hujoseph99@gmail.com>
2023-02-04 17:41:20 -0500

    refactor code to add direction in

--- Player.gd
+++ Player.gd
@@ -1,8 +1,15 @@
 extends KinematicBody2D
 
+enum Direction { UP, DOWN, LEFT, RIGHT }
+
 onready var _animated_sprite = $AnimatedSprite
+onready var direction = Direction.RIGHT
 export var speed = 400 # How fast the player will move (pixels/sec).
 
+func _ready():
+	# face right on start up
+	_animated_sprite.animation = "right" 
+	
 func get_velocity():
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_right"):
@@ -16,24 +23,34 @@ func get_velocity():
 		
 	return velocity.normalized() * speed
 
-func set_animation(motion):
+func set_direction(motion):
 	if motion.x < 0:
-		$AnimatedSprite.animation = "left"
+		direction = Direction.LEFT
 	elif motion.x > 0:
-		$AnimatedSprite.animation = "right"
+		direction = Direction.RIGHT
 	elif motion.y < 0:
-		$AnimatedSprite.animation = "up"
+		direction = Direction.UP
 	elif motion.y > 0:
-		$AnimatedSprite.animation = "down"
-		
-	if motion.length() > 0:
-		$AnimatedSprite.play()
-	else:
+		direction = Direction.DOWN
+
+func set_animation(stop_animation: bool):
+	if stop_animation:
 		$AnimatedSprite.stop()
+		return
+		
+	var dir = "right"
+	match direction:
+		Direction.UP: dir = "up"
+		Direction.DOWN: dir = "down"
+		Direction.LEFT: dir = "left"
+		Direction.RIGHT: dir = "right"
+	$AnimatedSprite.animation = dir
+	$AnimatedSprite.play()
 
 func _physics_process(delta):
 	var velocity = get_velocity()
 	var motion = velocity * delta 
 	
-	set_animation(motion)
+	set_direction(motion)
+	set_animation(motion.length() == 0)
 	move_and_collide(motion)
